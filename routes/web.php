ee053a1404d642bde2d3001f54d9447c574a194d
Spaces <49436788+SimpllleUser@users.noreply.github.com>
2020-01-20 20:29:15 +0200

    video 10 or 5 ' controller categories controller '

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
-});
-Route::get('/Test', function () {
-    return view('Test');
-});
+}); 
+Route::get('/admin','Admin\DashboardController@index');
+Route::resource('/admin/categories','Admin\CategoriesController');
