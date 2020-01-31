26bad53db9e4cb7aca94f88cfb572c9247cae9f8
SimpllleUser <pecheritsa.vv@gmail.com>
2020-01-31 20:27:58 +0200

    Fix page createUsers

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -6,101 +6,44 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Добавить статью
+        Добавить пользователя
         <small>приятные слова..</small>
       </h1>
     </section>
 
     <!-- Main content -->
     <section class="content">
-	{{Form::open([
-		'route'	=> 'posts.store',
-		'files'	=>	true
-	])}}
+		{{Form::open(['route'	=>	'users.store', 'files'	=>	true])}}
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">Добавляем статью</h3>
+          <h3 class="box-title">Добавляем пользователя</h3>
           @include('admin.errors')
         </div>
         <div class="box-body">
           <div class="col-md-6">
             <div class="form-group">
-              <label for="exampleInputEmail1">Название</label>
-              <input type="text" class="form-control" id="exampleInputEmail1" placeholder="" name="title" value="{{old('title')}}">
+              <label for="exampleInputEmail1">Имя</label>
+              <input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="" value="{{old('name')}}">
             </div>
-
             <div class="form-group">
-              <label for="exampleInputFile">Лицевая картинка</label>
-              <input type="file" id="exampleInputFile" name="image">
+              <label for="exampleInputEmail1">E-mail</label>
+              <input type="text" name="email" class="form-control" id="exampleInputEmail1" placeholder="" value="{{old('email')}}">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Пароль</label>
+              <input type="password" name="password" class="form-control" id="exampleInputEmail1" placeholder="">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputFile">Аватар</label>
+              <input type="file" name="avatar" id="exampleInputFile">
 
               <p class="help-block">Какое-нибудь уведомление о форматах..</p>
             </div>
-            <div class="form-group">
-              <label>Категория</label>
-              {{Form::select('category_id',
-              	$errors,
-              	null,
-              	['class' => 'form-control select2'])
-              }}
-            </div>
-            <div class="form-group">
-              <label>Теги</label>
-              {{Form::select('tags[]',
-              	$tags,
-              	null,
-              	['class' => 'form-control select2', 'multiple'=>'multiple','data-placeholder'=>'Выберите теги'])
-              }}
-            </div>
-            <!-- Date -->
-            <div class="form-group">
-              <label>Дата:</label>
-
-              <div class="input-group date">
-                <div class="input-group-addon">
-                  <i class="fa fa-calendar"></i>
-                </div>
-                <input type="date" class="form-control pull-right" id="datepicker" name="date" value="{{old('date')}}">
-              </div>
-              <!-- /.input group -->
-            </div>
-
-            <!-- checkbox -->
-            <div class="form-group">
-              <label>
-                <input type="checkbox" class="minimal" name="is_featured">
-              </label>
-              <label>
-                Рекомендовать
-              </label>
-            </div>
-
-            <!-- checkbox -->
-            <div class="form-group">
-              <label>
-                <input type="checkbox" class="minimal" name="status">
-              </label>
-              <label>
-                Черновик
-              </label>
-            </div>
-          </div>
-          <div class="col-md-12">
-            <div class="form-group">
-              <label for="exampleInputEmail1">Описание</label>
-              <textarea name="description" id="" cols="30" rows="10" class="form-control" >{{old('description')}}</textarea>
-          </div>
-        </div>
-          <div class="col-md-12">
-            <div class="form-group">
-              <label for="exampleInputEmail1">Полный текст</label>
-              <textarea name="content" id="" cols="30" rows="10" class="form-control" ></textarea>
-          </div>
         </div>
       </div>
         <!-- /.box-body -->
         <div class="box-footer">
-          <button class="btn btn-default">Назад</button>
           <button class="btn btn-success pull-right">Добавить</button>
         </div>
         <!-- /.box-footer-->
